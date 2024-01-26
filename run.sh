2f8f9a8425be3e45ceb61731e64a4cc37d2fda35
Kaleem Peeroo <mail@kaleempeeroo.com>
2024-01-26 18:09:58 +0000

    Updated run.sh to work with new logOutput flag

--- run.sh
+++ run.sh
@@ -12,6 +12,8 @@ login=$6
 metrics_file="$(hostname)-log"
 parcel_size=$7
 bootstrap=$8
+log_output_dir=$9
+
 echo $bootstrap
 nbNodes=$((builder + validator))
 ((nbNodes += regular))
@@ -38,32 +40,27 @@ systemctl start sysstat
 sar -A -o /home/$login/${ip} 1 $exp_duration >/dev/null 2>&1 &
 sleep 1
 
-
-
-
 # ========== Metrics Gathering Launch ==========
 echo "========== Metrics Gathering Launch =========="
 
 # ========== Experiment Launch ==========
-
-
 echo "========== Experiment Launch =========="
 
 # Run validator
 if [ "$validator" -ne 0 ]; then
     for ((i=0; i<$validator; i++)); do
-        go run . -duration="$experiment_duration" -nodeType=validator -size="$parcel_size" &
+        go run . -duration="$experiment_duration" -nodeType=validator -size="$parcel_size" -logOutput="$log_output_dir" &
         echo "validator $i"
         sleep 0.5
         ((nbNodes -= 1))
-        echo $nbNodes
-   done
+        echo $nbNodes   
+    done
 
     if [ "$builder" -eq 0 ] && [ "$regular" -ne 0 ]; then
-        go run . -duration"=$experiment_duration" -nodeType=validator -size="$parcel_size"
+        go run . -duration"=$experiment_duration" -nodeType=validator -size="$parcel_size" -logOutput="$log_output_dir"
     else
         if [ "$validator" -ne 1 ]; then
-            go run . -duration="$experiment_duration" -nodeType=validator -size="$parcel_size"&
+            go run . -duration="$experiment_duration" -nodeType=validator -size="$parcel_size" -logOutput="$log_output_dir"&
             sleep 0.5
             ((nbNodes -= 1))
             echo $nbNodes
@@ -74,7 +71,7 @@ fi
 # Run other nodes
 if [ "$regular" -ne 0 ]; then
     for ((i=0; i<$regular; i++)); do
-        go run . -duration="$experiment_duration" -nodeType=regular -size="$parcel_size" &
+        go run . -duration="$experiment_duration" -nodeType=regular -size="$parcel_size" -logOutput="$log_output_dir" &
         echo "regular $i"
         sleep 0.5
         ((nbNodes -= 1))
@@ -82,10 +79,10 @@ if [ "$regular" -ne 0 ]; then
     done
 
     if [ "$builder" -eq 0 ]; then
-        go run . -duration="$experiment_duration" -nodeType=regular -size="$parcel_size"
+        go run . -duration="$experiment_duration" -nodeType=regular -size="$parcel_size" -logOutput="$log_output_dir"
     else
         if [ "$regular" -ne 1 ]; then
-            go run . -duration="$experiment_duration" -nodeType=regular -size="$parcel_size" &
+            go run . -duration="$experiment_duration" -nodeType=regular -size="$parcel_size" -logOutput="$log_output_dir" &
             sleep 0.5
             ((nbNodes -= 1))
             echo $nbNodes
@@ -96,5 +93,5 @@ fi
 
 if [ "$builder" -ne 0 ]; then
     echo "builder launch"
-    go run . -duration="$experiment_duration" -nodeType=builder -size="$parcel_size"
+    go run . -duration="$experiment_duration" -nodeType=builder -size="$parcel_size" -logOutput="$log_output_dir"
 fi
